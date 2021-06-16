932b1bfc0cd92ffbb1ca32bfd677fc7d772f830b
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-06-16 22:18:16 +0300

    adding eslint airbnb, precommit and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>src/App.tsx</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
