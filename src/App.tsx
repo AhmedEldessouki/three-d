1e5ac690e5b1e2c2fdff36feace99aa95f8cb8ed
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-06-17 15:28:40 +0300

    useFrame isnt working.................

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react'
+import {useSpring, animated, config} from '@react-spring/three'
+import {Canvas, MeshProps} from '@react-three/fiber'
+import {Box, Html} from '@react-three/drei'
+
 import './App.css'
 
+function SomeThing() {
+  const [active, setActive] = React.useState(false)
+  const meshRef = React.useRef<MeshProps>()
+  const {scale} = useSpring({scale: active ? 1.5 : 1, config: config.wobbly})
+  return (
+    <div>
+      <Canvas>
+        <animated.mesh
+          scale={scale}
+          onClick={() => setActive(!active)}
+          ref={meshRef}
+        >
+          <boxGeometry />
+          <meshPhongMaterial color="red" />
+        </animated.mesh>
+        <mesh>
+          <Box position={[1, 10, 0]} />
+        </mesh>
+        <Html>
+          <h1>Mo</h1>
+        </Html>
+        <ambientLight intensity={0.1} />
+        <directionalLight />
+      </Canvas>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <SomeThing />
+      </Canvas>
     </div>
   )
 }
